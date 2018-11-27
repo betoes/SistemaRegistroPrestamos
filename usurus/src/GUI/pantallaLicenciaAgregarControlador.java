2039aebb99759ba96d3b47a4672d67be2892553c
jeth18 <36642059+jeth18@users.noreply.github.com>
2018-11-27 01:19:43 -0600

    Se ha agregado código de administar licencias

--- usurus/src/GUI/pantallaLicenciaAgregarControlador.java
+++ usurus/src/GUI/pantallaLicenciaAgregarControlador.java
@@ -1,7 +1,9 @@
 package GUI;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import domain.Licencia;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,6 +12,16 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class pantallaLicenciaAgregarControlador implements Initializable {
+
+  private String idLicencia;
+  private int numeroLicencias;
+  private Date fechaInicio;
+  private Date fechaFin;
+  private String clave;
+  private String proveedor;
+  private String caracter;
+  private String tipoLicenciamiento;
+  private Licencia licencia;
 
   @FXML
   private TextField txtidLicencia;
@@ -40,6 +52,19 @@ public class pantallaLicenciaAgregarControlador implements Initializable {
 
   @FXML
   private Button bSalir;
+
+  @FXML
+  public boolean agregarLicencia() {
+
+    boolean agregado = false;
+
+    idLicencia = txtidLicencia.getText();
+    numeroLicencias = Integer.parseInt(txtNoLicencias.getText());
+
+
+
+    return agregado;
+  }
 
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
